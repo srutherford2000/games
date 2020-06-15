9bf23ab7bb27fd83c6b664c48641c01bed821873
Sarah Rutherford <sarah.rutherford@westpoint.edu>
2020-06-15 12:10:47 -0400

    only one minor bug, next version will have bug fix and multiple different games

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,9 +1,11 @@
 import pgzrun ##for game stuff
-import math   ##for trig stuff
 import random ##for making randomized decisions
 import time
 
-mistake = 0 
+mistake = 0
+clock = time.monotonic()
+error = False
+error_msg = ''
 
 WIDTH=542
 LENGTH=550
@@ -64,16 +66,17 @@ chosen_block = ""
 start = False
 
 def check_square(num):
-    global mistake
+    global mistake, error, error_msg
     if sudoku[int(chosen_block)] == " ":
         if num ==solution[int(chosen_block)]:
             sudoku[int(chosen_block)] = num
         else:
             mistake += 1
-            print("that number was wrong!")
-            print("you now have", mistake, "mistakes.")
+            error_msg = "that number was wrong!"
+            error = True
     else:
-        print("this was a given number! don't try to change it")
+        error_msg ="this was a given number! don't try to change it"
+        error = True
 
 
 def check_numbers():
@@ -81,30 +84,39 @@ def check_numbers():
     if keyboard.k_1 and start:
             check_square("1")
             start = False
+            dict[chosen_block].image = 'square'
     elif keyboard.k_2 and start:
         check_square("2")
         start = False
+        dict[chosen_block].image = 'square'
     elif keyboard.k_3 and start:
         check_square("3")
         start = False
+        dict[chosen_block].image = 'square'
     elif keyboard.k_4 and start:
         check_square("4")
         start = False
+        dict[box].image = 'square'
     elif keyboard.k_5 and start:
         check_square("5")
         start = False
+        dict[chosen_block].image = 'square'
     elif keyboard.k_6 and start:
         check_square("6")
         start = False
+        dict[chosen_block].image = 'square'
     elif keyboard.k_7 and start:
         check_square("7")
         start = False
+        dict[chosen_block].image = 'square'
     elif keyboard.k_8 and start:
         check_square("8")
         start = False
+        dict[chosen_block].image = 'square'
     elif keyboard.k_9 and start:
         check_square("9")
         start = False
+        dict[chosen_block].image = 'square'
     
 
 def on_mouse_down(pos):
@@ -114,10 +126,12 @@ def on_mouse_down(pos):
             chosen_block = box
             start = True
 def draw():
-    global sudoku
+    global sudoku,start, chosen_block, mistake, clock,error,error_msg
     screen.fill("white")
     for box in dict:
             dict[box].draw()
+    if start:
+        dict[chosen_block].image = 'gray'
     vert1.draw()
     vert2.draw()
     hor1.draw()
@@ -128,7 +142,15 @@ def draw():
     hor3.draw()
     for i, num in enumerate(sudoku):
         screen.draw.text(num, (23+((i%9)*60),20+((i//9)*60)),color="black", fontsize=30)
-        
+    if error:
+        print("here")
+        screen.draw.text(error_msg,(20,563),color="red", fontsize=30)
+        time.sleep(2)
+        error = False
+    else:
+        screen.draw.text("You have "+str(mistake)+" mistakes",(20,563),color="black", fontsize=30)
+        screen.draw.text("Time: "+str(int(time.monotonic()-clock)),(450,563),color="black", fontsize=30)
+
 
 def update():
     check_numbers()
